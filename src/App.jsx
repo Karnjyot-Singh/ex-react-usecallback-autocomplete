7e769e51e7bd39e482981d06b3ff1c5ff7319d85
Karnjyot <krnjyotsingh2004@email.com>
2025-04-29 18:12:15 +0200

    fine esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useCallback } from 'react'
+
+const debounce = (callback, delay) =>{
+  let timeout
+  return(vlaue) => {
+    clearTimeout(timeout)
+    timeout= setTimeout(() => {
+      callback(value)
+    }, delay)
+  }
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("")
+
+  const[suggestions, setSuggestions] = useState([])
+
+  const fetchProducts = async (query) => {
+    if(!query.trim()){
+      setSuggestions([])
+      return
+     }
+     try{
+      const res = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`)
+      const data = await res.json
+      setSuggestions(data)
+      console.log('API')
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  const debouncedFetchProducts = useCallback(
+     debounce(fetchProducts, 500)
+  , [])
+
+  //uso useEffect perché devo dire di fare un'opearzione ad ogni cambio di uno stato
+  useEffect(() => {
+    debouncedFetchProducts(query)
+  }, [query])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <h1>Autocomplete</h1>
+        <input
+           type='text'
+           placeholder='cerca'
+           value={query}
+           onChange={e => setQuery(e.target.value)} 
+        />
+        {suggestions.length > 0 && (
+          <div className="dropdown">
+            {suggestions.map((product) => (
+              <p key={product.id}>{product.name}</p>
+            ))}
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
